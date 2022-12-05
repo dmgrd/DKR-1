1cf664f08fbda9b76489e4a2576a658714104aff
dmgrd <117730998+dmgrd@users.noreply.github.com>
2022-12-05 14:20:37 +0300

    Add files via upload

---
+++ DKR 1 option 2 code.pas
@@ -0,0 +1,15 @@
+﻿Program dkr1option2;
+var a,b,x,y,h:real;
+begin
+  writeln('Введите интервал промежутка a и b и шаг h:');
+  read(a,b,h);
+  x:=a;
+  while x<=b do 
+    begin
+  if x<-9 then y:=(-x*power(x,1/3))+(x/power(x,0.1*x))
+  else if (x<-4) then y:=(exp(x)*power(x,0.1*x)+(-x/power(x,3)))
+  else y:=(x/cos(2*x));
+    writeln('x = ', x:1:2, ' y = ',y:1:2);
+    x+=h;
+    end;
+end.
